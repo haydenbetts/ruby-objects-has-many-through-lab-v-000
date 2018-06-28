66e2f16c23816f1ea4e473aa907c7f0c7ef43802
Hayden Betts <haydenbetts@gmail.com>
2018-06-28 21:41:03 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,8 +1,15 @@
 class Appointment
-  attr_accessor :
-  
+  attr_accessor :date, :patient, :doctor
+
   @@all = []
+
   def self.all
     @@all
   end
+
+  def initialize(date, patient, doctor)
+    self.date = date
+  end
+
+
 end
