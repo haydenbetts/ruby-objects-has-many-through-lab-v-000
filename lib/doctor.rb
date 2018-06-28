96007c2924ffe96e4e38fd52e06cdcdcc5e93c34
Hayden Betts <haydenbetts@gmail.com>
2018-06-28 21:54:21 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,11 +18,11 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select {|appointment| appointment.doctor = doctor}
+    Appointment.all.select {|appointment| appointment.doctor = self.doctor}
   end
 
   def patients
-    self.appointments.collect {|appointment| self.patient}
+    self.appointments.collect {|appointment| appointment.patient}
   end
 
 end
